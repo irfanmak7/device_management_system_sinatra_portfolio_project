09dc008d049424e8302402022dc6af22e6a22b44
Irfan Maknojia <imak_7@yahoo.com>
2019-04-15 19:59:38 -0500

    add configure method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < Sinatra::Base
 
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "tims_app"
+    end
 end
