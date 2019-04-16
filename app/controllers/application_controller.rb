725fe9d76381bfaff3c1e68045d00be4dcad992f
Irfan Maknojia <imak_7@yahoo.com>
2019-04-15 20:00:41 -0500

    update configure method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < Sinatra::Base
         set :public_folder, 'public'
         set :views, 'app/views'
         enable :sessions
-        set :session_secret, "tims_app"
+        set :session_secret, "device_management_system_app"
     end
 end
