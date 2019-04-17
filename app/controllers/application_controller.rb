4510664784f113df46cd5cd42aca646ef842ae1b
Irfan Maknojia <imak_7@yahoo.com>
2019-04-16 21:48:22 -0500

    create logout! helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,10 @@ class ApplicationController < Sinatra::Base
         def current_user
             @current_user ||= User.find_by(id: session[:user_id])
         end
+
+        def logout!
+
+        end
     
     end
 end
