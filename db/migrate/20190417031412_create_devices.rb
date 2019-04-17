334a865749f77812c96a2160fe8cc35d3608e011
Irfan Maknojia <imak_7@yahoo.com>
2019-04-16 22:17:37 -0500

    add name, serial number, color and user_id to device

--- db/migrate/20190417031412_create_devices.rb
+++ db/migrate/20190417031412_create_devices.rb
@@ -1,8 +1,10 @@
 class CreateDevices < ActiveRecord::Migration[5.2]
   def change
     create_table :devices do |t|
-      
-
+      t.string :name
+      t.string :serial_number
+      t.string :color
+      t.integer :user_id
     end
   end
 end
