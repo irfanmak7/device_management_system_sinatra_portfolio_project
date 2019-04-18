261884bc2d61fcb859239b0d2731a31ef633c759
Irfan Maknojia <imak_7@yahoo.com>
2019-04-17 20:45:24 -0500

    add if/else statement to save new device

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -10,7 +10,20 @@ class DevicesController < ApplicationController
     end
 
     post '/devices' do
-
+        if logged_in?
+            if params[:name] == "" || params[:serial_number] == "" || params[:color] == ""
+                redirect to "/devices/new"
+            else
+                @device = current_user.devices.build(name: params[:name], serial_number: params[:serial_number], color: params[:color])
+                if @device.save
+                    redirect to "/devices/#{@device.id}"
+                else
+                    redirect to "/devices/new"
+                end
+            end
+        else
+            redirect to '/login'
+        end
     end
 
 end
