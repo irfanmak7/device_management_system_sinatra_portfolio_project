c1152312e57a5a8e30dfce64d45a58a79236aee2
Irfan Maknojia <imak_7@yahoo.com>
2019-04-18 22:38:26 -0500

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password
 
-    validates :name, :email, presence: true
+    validates :name, :email,:password, presence: true
+    validates :email, uniqueness: true
 
     has_many :devices
 end
